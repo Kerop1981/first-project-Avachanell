fe70bebd12891612654a5159f7e7e71773c9379c
Kerop1981 <vova.abraamyan@mail.ru>
2024-03-02 07:26:58 +0300

    problem

---
+++ src/app/createedituser/createedituser.component.ts
@@ -0,0 +1,37 @@
+import { Component, Inject} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+@Component({
+  selector: 'createedituser',
+  standalone: true,
+  imports: [ MatFormFieldModule,ReactiveFormsModule,MatDialogModule],
+  templateUrl: './createedituser.component.html',
+  styleUrl: './createedituser.component.css'
+})
+export class CreateedituserComponent {
+  userForm!: FormGroup;
+
+  constructor(
+    private dialogRef : MatDialogRef<CreateedituserComponent>,
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: {isEdit: boolean}
+  ){
+    this.userForm = this.fb.group({
+      name:'',
+      email: '',
+      phone: '',
+    });
+  }
+
+  onCancelClick(): void {
+    this.dialogRef.close()
+  }
+
+  onSaveClick(): void {
+    this.dialogRef.close(this.userForm.value)
+  }
+}
+
--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,22 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { User, UsersApiService } from '../service/users-api.service';
+import { Component, OnInit } from '@angular/core';
+import { User, UsersApiService,} from '../service/users-api.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateedituserComponent } from '../createedituser/createedituser.component';
+import { Observable } from 'rxjs';
 import { UserService } from '../service/user.service';
 import { UserCardComponent } from "../user-card/user-card.component";
 import { CommonModule } from '@angular/common';
-import { Observable, map } from 'rxjs';
 @Component({
     selector: 'users-list',
     standalone: true,
     templateUrl: './users-list.component.html',
     styleUrl: './users-list.component.css',
-    imports: [CommonModule, UserCardComponent]
+    imports: [CreateedituserComponent, UserCardComponent,CommonModule]
 })
 export class UsersListComponent implements OnInit{
   users!: Observable<User[]>
 
 constructor(
   private UsersApiService:UsersApiService,
-  private UserService:UserService){ }
+  private UserService:UserService,
+  private dialog: MatDialog,
+  ){ }
 
 
 ngOnInit(): void {
@@ -26,7 +30,15 @@ ngOnInit(): void {
 
 deleteUser(id: number): void {
   this.UserService.deleteUser(id);
-  this.users = this.users.pipe(map(users => users.filter(user => user.id !== id))
-  );
+  
 }
-}
+
+openMatDialog(){
+  this.UsersApiService.getUsers().subscribe(users => {
+    const dialogRef = this.dialog.open(CreateedituserComponent,{
+      width: '300px',
+      data:{id: users.length + 1 }
+    });
+  });
+ }
+} 
